5b6d645fbd1450a23bf39553d4014b582d6b85ae
Maxime Vanliefde <maxvanliefde@proton.me>
2023-12-04 23:54:01 -0500

    script fix :-)

--- launch-nworkers.sh
+++ launch-nworkers.sh
@@ -7,14 +7,19 @@ files=(
 mkdir -p results-ncores
 
 max_workers=$(($(nproc) * 2))
-
 for file in "${files[@]}"; do
   instance=$(echo "$file" | sed 's/.*\///' | sed 's/\..*//')
   result_file="results-ncores/results-ncores-$instance.csv"
-  echo "nworkers,time" > "$result_file"
+  echo "nworkers,time1,time2,time3,time4,time5" > "$result_file"
   for nworkers in $(seq 1 "$max_workers"); do
-    echo "Running $nworkers workers on $instance"
-    timeout -v 3660 /usr/bin/time -f %e java -jar target/minicpbp-1.0.jar --input "$file" --branching dom-wdeg --search-type dfs --timeout 3600 --workers "$nworkers" 2>&1 > /dev/null
-    echo "$nworkers,$time" >> "$result_file"
+    echo -n "Running $nworkers workers on $instance"
+    echo -n "$nworkers" >> "$result_file"
+    for i in $(seq 1 5); do
+      echo -n "."
+      time=$(timeout -v 3660 /usr/bin/time -f %e java -jar target/minicpbp-1.0.jar --input "$file" --branching dom-wdeg --search-type dfs --timeout 3600 --workers "$nworkers" 2>&1 > /dev/null)
+      echo -n ",$time" >> "$result_file"
+    done
+    echo "" >> "$result_file"
+    echo " done"
   done
 done
